b63fb85d76650454d342360de8c32952652ea098
Levi Blackstone <levi@pulumi.com>
2021-06-28 14:20:26 -0600

    Address diff feedback

--- pkg/resource/deploy/step_generator.go
+++ pkg/resource/deploy/step_generator.go
@@ -524,18 +524,16 @@ func (sg *stepGenerator) generateStepsFromDiff(
 			"unrecognized diff state for %s: %d", urn, diff.Changes)
 	}
 
-	// There are changes if the diff returned DiffSome or if there were initErrors previously that should
-	// force an update to occur even if no inputs changed.
 	hasInitErrors := len(old.InitErrors) > 0
-	needsUpdateOrReplace := diff.Changes == plugin.DiffSome || hasInitErrors
+
+	// Update the diff to apply any replaceOnChanges annotations and to include initErrors in the diff.
+	diff, err = sg.applyReplaceOnChanges(diff, goal.ReplaceOnChanges, hasInitErrors)
+	if err != nil {
+		return nil, result.FromError(err)
+	}
 
 	// If there were changes check for a replacement vs. an in-place update.
-	if needsUpdateOrReplace {
-		// Update the diff to apply any replaceOnChanges annotations and to include initErrors in the diff.
-		diff, err = sg.applyReplaceOnChanges(diff, goal.ReplaceOnChanges, hasInitErrors)
-		if err != nil {
-			return nil, result.FromError(err)
-		}
+	if diff.Changes == plugin.DiffSome {
 		if diff.Replace() {
 			// If the goal state specified an ID, issue an error: the replacement will change the ID, and is
 			// therefore incompatible with the goal state.
@@ -1190,17 +1188,13 @@ func (sg *stepGenerator) getProviderResource(urn resource.URN, provider string) 
 const initErrorSpecialKey = "#initerror"
 
 // applyReplaceOnChanges adjusts a DiffResult returned from a provider to apply the ReplaceOnChange
-// settings in the desired state and initerrors from the previous state.
+// settings in the desired state and init errors from the previous state.
 func (sg *stepGenerator) applyReplaceOnChanges(diff plugin.DiffResult,
 	replaceOnChanges []string, hasInitErrors bool) (plugin.DiffResult, error) {
 
-	var err error
-	diffPaths := map[string]resource.PropertyPath{}
-	for p := range diff.DetailedDiff {
-		diffPaths[p], err = resource.ParsePropertyPath(p)
-		if err != nil {
-			return diff, err
-		}
+	// No further work is necessary for DiffNone unless init errors are present.
+	if diff.Changes != plugin.DiffSome && !hasInitErrors {
+		return diff, nil
 	}
 
 	var replaceOnChangePaths []resource.PropertyPath
